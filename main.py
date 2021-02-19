efbc6406f41acf065d157f9c32c4989f0cc89769
Giancarlo <giancarlo@pop-os.localdomain>
2021-02-19 09:00:17 -0500

    grid layout imported

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import kivy
 from kivy.app import App 
 from kivy.uix.label import Label    
-
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 class MyApp(App):
     def build(self):
         return Label(text = "Tech with Giancarlo")

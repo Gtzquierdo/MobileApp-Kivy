8aabfa2ae3680ffd5cd12bb648f9efa651b4ac3e
Giancarlo <giancarlo@pop-os.localdomain>
2021-02-20 21:56:25 -0500

    First and last name widgets and email widgets created

--- main.py
+++ main.py
@@ -3,9 +3,28 @@ from kivy.app import App
 from kivy.uix.label import Label    
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+
+#  Creating Grid Layout 
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        #  Adding elements - columns
+        self.cols = 2 #Defining the amount of columns to be 2
+        self.add_widget(Label(text="First Name: ")) # Add a label widget
+        self.name = TextInput(multiline=False) # Create a Text input box stored in the name variable
+        self.add_widget(self.name) # Add the text input widget to the GUI
+
+        self.add_widget(Label(text="Last Name: ")) # Add a label widget
+        self.name = TextInput(multiline=False) # Create a Text input box stored in the name variable
+        self.add_widget(self.lastName) # Add the text input widget to the GUI
+
+        self.add_widget(Label(text="Email: ")) # Add a label widget
+        self.name = TextInput(multiline=False) # Create a Text input box stored in the name variable
+        self.add_widget(self.email) # Add the text input widget to the GUI
+
 class MyApp(App):
     def build(self):
-        return Label(text = "Tech with Giancarlo")
+        return MyGrid()
 
 # Created window -low level for test-
 if __name__ == "__main__":
